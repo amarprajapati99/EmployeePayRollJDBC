ea46b98e547f50020a1d2241564f5e31337e39f1
amarprajapati99 <amarprajapati99@gmail.com>
2021-04-29 00:22:05 +0530

    Welcome

---
+++ src/main/java/com/employee/payrollservices/EmployeePayRollServices.java
@@ -0,0 +1,7 @@
+package com.employee.payrollservices;
+
+public class EmployeePayRollServices {
+    public static void main(String[] args) {
+        System.out.println("Welcome to the employee payroll services");
+    }
+}
