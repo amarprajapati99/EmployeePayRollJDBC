b3b6a111f2f97294b7ac18de7d3a62fcb4abf5c4
amarprajapati99 <amarprajapati99@gmail.com>
2021-04-29 01:16:07 +0530

    Added : Employee payroll service to retrive the employee payroll

--- src/test/java/com/employee/payrollservices/EmployeePayrollServicesTest.java
+++ src/test/java/com/employee/payrollservices/EmployeePayrollServicesTest.java
@@ -1,6 +1,19 @@
 package com.employee.payrollservices;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
 public class EmployeePayrollServicesTest {
-    EmployeePayRollServices employeePayRollServices = new EmployeePayRollServices();
+    EmployeePayrollService employeePayrollService;
+    List<EmployeePayrollData> employeePayrollList;
+
+    @Test
+    public void givenEmployeePayrollInDB_WhenRetrived_ShouldMatchEmployeeCount() {
+        employeePayrollService = new EmployeePayrollService();
+        employeePayrollList = employeePayrollService.readEmployeePayrollData(EmployeePayrollService.IOService.DB_IO);
+        Assertions.assertEquals(3, employeePayrollList.size());
+    }
 
 }
